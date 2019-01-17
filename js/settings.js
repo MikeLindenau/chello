4fbb499d9f0fe03e99056d5ea33bf4adc721bd16
Michael Lindenau <mike@gumspace.com>
2019-01-16 20:24:00 -0800

    add log

--- js/settings.js
+++ js/settings.js
@@ -1,36 +1,40 @@
 /* global TrelloPowerUp */
 
-var Promise = TrelloPowerUp.Promise;
-var t = TrelloPowerUp.iframe();
+var Promise = TrelloPowerUp.Promise
+var t = TrelloPowerUp.iframe()
 
-var fruitSelector = document.getElementById('fruit');
-var vegetableSelector = document.getElementById('vegetable');
+var fruitSelector = document.getElementById('fruit')
+var vegetableSelector = document.getElementById('vegetable')
 
-t.render(function(){
+t.board('id', 'name', 'url', 'shortLink', 'members').then(function(data) {
+  console.log({ members: data })
+})
+
+t.render(function() {
   return Promise.all([
     t.get('board', 'shared', 'fruit'),
     t.get('board', 'private', 'vegetable')
   ])
-  .spread(function(savedFruit, savedVegetable){
-    if(savedFruit && /[a-z]+/.test(savedFruit)){
-      fruitSelector.value = savedFruit;
-    }
-    if(savedVegetable && /[a-z]+/.test(savedVegetable)){
-      vegetableSelector.value = savedVegetable;
-    }
-  })
-  .then(function(){
-    t.sizeTo('#content')
-    .done();
-  })
-});
+    .spread(function(savedFruit, savedVegetable) {
+      if (savedFruit && /[a-z]+/.test(savedFruit)) {
+        fruitSelector.value = savedFruit
+      }
+      if (savedVegetable && /[a-z]+/.test(savedVegetable)) {
+        vegetableSelector.value = savedVegetable
+      }
+    })
+    .then(function() {
+      t.sizeTo('#content').done()
+    })
+})
 
-document.getElementById('save').addEventListener('click', function(){
-  return t.set('board', 'private', 'vegetable', vegetableSelector.value)
-  .then(function(){
-    return t.set('board', 'shared', 'fruit', fruitSelector.value);
-  })
-  .then(function(){
-    t.closePopup();
-  })
+document.getElementById('save').addEventListener('click', function() {
+  return t
+    .set('board', 'private', 'vegetable', vegetableSelector.value)
+    .then(function() {
+      return t.set('board', 'shared', 'fruit', fruitSelector.value)
+    })
+    .then(function() {
+      t.closePopup()
+    })
 })
